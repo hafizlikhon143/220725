518c2827bc1513081acb7d68d8fd688befb1fd34
Nurul Hafiz Likhon <67749565+hafizlikhon143@users.noreply.github.com>
2022-07-26 15:12:32 +0600

    Create prob-3.kt
    Exercise no 3

---
+++ ABC/Path1/Act-3/9-Exercises/prob-3.kt
@@ -0,0 +1,22 @@
+/*
+ * Codede 3
+ * 
+ */
+
+fun main() {
+    println("Monday")
+    println("Tuesday")
+    println("Wednesday")
+    println("Thursday")
+    println("Friday")
+}
+
+// Output
+
+
+// Monday
+// Tuesday
+// Wednesday
+// Thursday
+// Friday
+
